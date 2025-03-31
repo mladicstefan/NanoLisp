a1bed841ba4c617c18f383f3b37ddd46ac93cba8
mladicstefan <mladicstefan6@gmail.com>
2025-03-31 15:19:53 +0200

    Recursive Eval Function for Parser

--- main.c
+++ main.c
@@ -29,6 +29,23 @@ long eval_op(long x, char* op,long y){
   return 0;
 }
 
+long eval(mpc_ast_t* t){
+  
+  if (strstr(t->tag,"number")) {
+    return atoi(t->contents);
+  }
+  
+  char* op = t->children[1]->contents;
+  long x = eval(t->children[2]);
+  
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")){
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+  return x;
+}
+
 int main(int argc, char** argv){
   return 0;
 }
